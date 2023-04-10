b5aa482ead78f8e306663b6c37e81b1eed9743f1
rkez-dys <130051276+rkez-dys@users.noreply.github.com>
2023-04-10 19:19:41 +0900

    Update sample_code.tsx

--- sample_code.tsx
+++ sample_code.tsx
@@ -1,15 +1,20 @@
-const uploadFile = async (file: File) => {
-    const headers = {
-      'Content-Type': file.type,
-      'Content-Disposition': `attachment; filename="${file.name}"`
-    };
-  
-    const response = await fetch('/api/upload', {
-      method: 'POST',
-      body: file,
-      headers: headers
-    });
-  
-    const data = await response.json();
-    return data;
-  };
+import React from 'react';
+
+class MyComponent extends React.Component {
+  clickEvent = (e: React.MouseEvent<HTMLLIElement>) => {
+    e.preventDefault(); // デフォルトの動作をキャンセルする
+    // HTML のバリデーションを行うコードを記述する
+    const form = e.currentTarget.form;
+    if (form.checkValidity()) {
+      console.log("HTML is valid");
+    } else {
+      console.log("HTML is invalid");
+    }
+  }
+
+  render() {
+    return (
+      <li onClick={(e) => this.clickEvent(e)}>Click me!</li>
+    );
+  }
+}
