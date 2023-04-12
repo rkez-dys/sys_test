81456c8c3e838da9e87e4d5a75f70660f1c28423
rkez-dys <130051276+rkez-dys@users.noreply.github.com>
2023-04-12 19:26:30 +0900

    Update sample_code.tsx

--- sample_code.tsx
+++ sample_code.tsx
@@ -1,31 +1,18 @@
-import React from 'react';
+import React, { useState, useRef } from "react";
 
-class MyComponent extends React.Component {
-  clickEvent = (e: React.MouseEvent<HTMLLIElement>) => {
-    e.preventDefault(); // デフォルトの動作をキャンセルする
-    // HTML のバリデーションを行うコードを記述する
-    const form = e.currentTarget.form;
-    if (form.checkValidity()) {
-      console.log("HTML is valid");
-    } else {
-      console.log("HTML is invalid");
-    }
+const Test = () => {
+  const [data, setData] = useState<string>('');
+  const textRef = useRef<HTMLInputElement>(null);
+
+  const handleDataChange = () => {
+    setData(textRef.current?.value ?? '');
   }
 
-  render() {
-    return (
-      <li onClick={(e) => this.clickEvent(e)}>Click me!</li>
-    );
-  }
-}
-
-<li onClick={(e) => clickEvent(e)} style={{ display: 'list-item' } />
-
-const liClickEvent = () => {
-  if (buttonRef.current) {
-    buttonRef.current.click();
-  }
+  return (
+    <div>
+      <input type="text" ref={textRef} onChange={handleDataChange} />
+    </div>
+  );
 };
 
- <button type="submit" ref={buttonRef} style={{ display: "none" }} />
- <li onClick={liClickEvent}>Click me</li>
+export default Test;
