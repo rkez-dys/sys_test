100cf1b73659fe83d8dd9c3db072349e69fd4e3a
rkez-dys <130051276+rkez-dys@users.noreply.github.com>
2023-04-21 18:56:19 +0900

    Update sample_code.tsx

--- sample_code.tsx
+++ sample_code.tsx
@@ -21,3 +21,31 @@ rootElement.appendChild(childElement2);
 const xmlString = new XMLSerializer().serializeToString(xmlDocument);
 
 console.log(xmlString);
+
+interface MyData {
+  id: number;
+  name: string;
+  age: number;
+}
+
+const myDataList: MyData[] = [
+  { id: 1, name: 'Alice', age: 25 },
+  { id: 2, name: 'Bob', age: 30 },
+  { id: 3, name: 'Charlie', age: 35 },
+  { id: 4, name: 'David', age: 40 },
+  { id: 5, name: 'Emily', age: 45 },
+  { id: 6, name: 'Frank', age: 50 },
+];
+
+const pageSize = 2; // 1ページあたりのデータ数
+
+const getPage = (pageNumber: number, data: MyData[]): MyData[] => {
+  const startIndex = (pageNumber - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  return data.slice(startIndex, endIndex);
+};
+
+const currentPage = 2; // 現在のページ
+const currentData = getPage(currentPage, myDataList); // 現在のページに表示するデータ
+
+console.log(currentData); // [{ id: 3, name: 'Charlie', age: 35 }, { id: 4, name: 'David', age: 40 }]
